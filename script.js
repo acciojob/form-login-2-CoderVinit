2b89caf0a8ab83f19661b7e1e2563ab183989f4e
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-01 11:05:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+document.getElementById("userForm").addEventListener("click",(e)=>{
+	e.preventDefault();
+
+	let firstName = document.getElementById("firstName").value.trim();
+    let lastName = document.getElementById("lastName").value.trim();
+    let phoneNumber = document.getElementById("phoneNumber").value.trim();
+    let email = document.getElementById("email").value.trim();
+
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`);
+})
